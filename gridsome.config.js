ea84d208ac1cdadcfc489a56833b7988a3aecc88
kanesada2 <kanesada2@users.noreply.github.com>
2022-06-22 17:23:11 +0900

    gh-pages用調整

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: '研修日誌',
+  siteDescription: '研修日誌',
   siteUrl: 'https://kanesada2.github.io',
   pathPrefix: '/agri-diary',
   templates: {
