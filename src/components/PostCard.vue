d7164d92a5a085b82ed5852bc2cf1990406c75ca
kanesada2 <kanesada2@users.noreply.github.com>
2023-01-12 19:41:33 +0900

    二重出力問題のアタリを付ける

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -10,9 +10,8 @@
 <!--
       <PostMeta class="post-card__meta" :post="post" />
       <PostTags class="post-card__tags" :post="post" />
--->
       <g-link class="post-card__link" :to="post.path">Link</g-link>
-
+!-->
     </div>
   </div>
 </template>
